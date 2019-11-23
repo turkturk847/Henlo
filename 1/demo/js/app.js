2e53eb382bf71587cbfafafdc2198da60a84736c
turkturk847 <56753559+turkturk847@users.noreply.github.com>
2019-11-23 11:46:03 +0200

    153153

--- 1/demo/js/app.js
+++ 1/demo/js/app.js
@@ -63,7 +63,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "05FEFE",
+        "color": "RB8500",
         "opacity": 0.4,
         "width": 1
       },
